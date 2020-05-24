7f966581f1eb912f66f93d125c945aa85d51ade1
Damian Lupa <damian.lupa88@gmail.com>
2020-05-24 20:10:49 +0200

    Navigation2 component draft

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 import Nav from "./Nav";
 import Navigation from "./Navigation";
 import HomePage from "./HomePage";
 import Footer from "./Footer";
 
+import Navigation2 from "./Navigation2";
+
 function App() {
   return (
     <div>
-    
-        <Nav />
-        <Navigation />
-       
-        <HomePage />
-        <Footer />
-     
+      <Nav />
+      <Navigation />
+      <Navigation2 />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navigation2.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "../styles/Navigation2.css";
+import $ from "jquery";
+
+function Navigation2() {
+  var $nav = $(".greedy-nav");
+  var $btn = $(".greedy-nav button");
+  var $vlinks = $(".greedy-nav .visible-links");
+  var $hlinks = $(".greedy-nav .hidden-links");
+
+  var breaks = [];
+
+  function updateNav() {
+    var availableSpace = $btn.hasClass("hidden")
+      ? $nav.width()
+      : $nav.width() - $btn.width() - 30;
+
+    // The visible list is overflowing the nav
+    if ($vlinks.width() > availableSpace) {
+      // Record the width of the list
+      breaks.push($vlinks.width());
+
+      // Move item to the hidden list
+      $vlinks.children().last().prependTo($hlinks);
+
+      // Show the dropdown btn
+      if ($btn.hasClass("hidden")) {
+        $btn.removeClass("hidden");
+      }
+
+      // The visible list is not overflowing
+    } else {
+      // There is space for another item in the nav
+      if (availableSpace > breaks[breaks.length - 1]) {
+        // Move the item to the visible list
+        $hlinks.children().first().appendTo($vlinks);
+        breaks.pop();
+      }
+
+      // Hide the dropdown btn if hidden list is empty
+      if (breaks.length < 1) {
+        $btn.addClass("hidden");
+        $hlinks.addClass("hidden");
+      }
+    }
+
+    // Keep counter updated
+    $btn.attr("count", breaks.length);
+
+    // Recur if the visible list is still overflowing the nav
+    if ($vlinks.width() > availableSpace) {
+      updateNav();
+    }
+  }
+
+  // Window listeners
+
+  $(window).resize(function () {
+    updateNav();
+  });
+
+  $btn.on("click", function () {
+    $hlinks.toggleClass("hidden");
+  });
+
+  updateNav();
+
+  return (
+    <nav class="greedy-nav mt-5">
+      {/* <button>
+        <div class="hamburger"></div>
+      </button> */}
+      <div class="container-xl">
+        <ul class="visible-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Doctors</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <ul class="hidden-links hidden"></ul>
+    </nav>
+  );
+}
+
+export default Navigation2;
