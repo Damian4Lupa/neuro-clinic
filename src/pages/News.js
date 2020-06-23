77d1ede35aa8c161b4a429e7575f40a5a76e18ba
Damian Lupa <damian.lupa88@gmail.com>
2020-06-23 22:13:16 +0200

    News page draft

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,14 +1,246 @@
 import React from "react";
 import "../styles/GlobalStyle.css";
 
+import "../styles/pages/News.css";
+
 import test from "../img/test.jpg";
 
 function News() {
   return (
     <section id="news" className="home-section">
       <div className="container">
-        <div className="row">
-          <div class="card-deck text-center">
+        <div class="row">
+
+
+          {/* początek art */}
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* koniec art */}
+
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate fadeInUp ftco-animated">
+            <div class="blog-entry">
+              <div class="news-img">
+                <img src={test} alt="" class="block-20" />
+                <div class="meta-date text-center p-2">
+                  <span class="day">23</span>
+                  <span class="mos">January</span>
+                  <span class="yr">2019</span>
+                </div>
+              </div>
+              <div class="text bg-white p-4">
+                <h3 class="heading">
+                  <a href="#">Scary Thing That You Don’t Get Enough Sleep</a>
+                </h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+                <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0">
+                    <a href="#" class="btn btn-primary">
+                      Read More{" "}
+                      <span class="ion-ios-arrow-round-forward"></span>
+                    </a>
+                  </p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">
+                      Admin
+                    </a>
+                    <a href="#" class="meta-chat">
+                      <span class="icon-chat"></span> 3
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div class="card-deck text-center">
             <div class="card">
               <img class="card-img-top" src={test} alt="Card image cap" />
               <div className="card-header">
@@ -70,8 +302,7 @@ function News() {
                 <span>read more</span>
               </div>
             </div>
-          </div>
-        </div>
+          </div> */}
       </div>
     </section>
   );
