d7a87cce61c3dea533fc1e631fb7cd421ef51115
Damian Lupa <damian.lupa88@gmail.com>
2020-07-08 22:46:16 +0200

    Counter component end

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
-import React from "react";
-// import "../styles/GlobalStyle.css";
+import React, { useState } from "react";
+import $ from "jquery";
+import CountUp from "react-countup";
 import "../styles/components/Counter.css";
 
 import awards from "../img/Counter/awards.svg";
@@ -8,6 +9,25 @@ import doctors from "../img/Counter/doctors.svg";
 import research from "../img/Counter/research.svg";
 
 function Counter() {
+  const [count, setCount] = useState(false);
+
+  $(window).scroll(function () {
+
+    let scrollValue = $(document).scrollTop();
+    let counterIdTopValue = $("#counter").offset().top - 550;
+
+    if (scrollValue > counterIdTopValue) {
+      setCount(true);
+    } else {
+      setCount(false);
+    }
+  });
+
+  let doctorsCounterValue = count ? <CountUp end={20} /> : 0;
+  let departmentsCounterValue = count ? <CountUp end={18} /> : 0;
+  let researchCounterValue = count ? <CountUp end={8} /> : 0;
+  let awardsCounterValue = count ? <CountUp end={43} /> : 0;
+
   return (
     <section id="counter" className="home-section">
       <div className="container">
@@ -15,7 +35,7 @@ function Counter() {
           <div className="col-lg-3 col-md-6 d-md-flex">
             <div className="counter-box">
               <img src={doctors} alt="doctors" />
-              <span data-toggle="counter-up">85</span>
+              <span data-toggle="counter-up">{doctorsCounterValue}</span>
               <p>
                 <strong>Doctors</strong> consequuntur quae qui deca rode
               </p>
@@ -25,7 +45,7 @@ function Counter() {
           <div className="col-lg-3 col-md-6 d-md-flex">
             <div className="counter-box">
               <img src={departments} alt="departments" />
-              <span data-toggle="counter-up">18</span>
+              <span data-toggle="counter-up">{departmentsCounterValue}</span>
               <p>
                 <strong>Departments</strong> adipisci atque cum quia aut numquam
                 delectus
@@ -36,7 +56,7 @@ function Counter() {
           <div className="col-lg-3 col-md-6 d-md-flex">
             <div className="counter-box">
               <img src={research} alt="research" />
-              <span data-toggle="counter-up">8</span>
+              <span data-toggle="counter-up">{researchCounterValue}</span>
               <p>
                 <strong>Research Lab</strong> aut commodi quaerat. Aliquam
                 ratione
@@ -47,7 +67,7 @@ function Counter() {
           <div className="col-lg-3 col-md-6 d-md-flex">
             <div className="counter-box">
               <img src={awards} alt="awards" />
-              <span data-toggle="counter-up">150</span>
+              <span data-toggle="counter-up">{awardsCounterValue}</span>
               <p>
                 <strong>Awards</strong> rerum asperiores dolor molestiae
                 doloribu
